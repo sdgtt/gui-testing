c5932d0d4ed187eccd400458961d4f47602adcc8
Hannah Rosete <Hannah.Rosete@analog.com>
2021-11-03 08:46:11 +0800

    Add setup.py
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pyautogui-adi",
+    # version="",
+    description="ADI's UI Test using PyAutoGUI",
+    url="https://github.com/HRosete/pyautogui-adi.git",
+    author="Hannah Mae Nicole Rosete",
+    author_email="Hannah.Rosete@analog.com",
+    # license="",
+    packages=setuptools.find_packages(),
+)
