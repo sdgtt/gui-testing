890c982337fb98c3545a2e568c2a8a74baf7102a
Hannah Rosete <Hannah.Rosete@analog.com>
2022-10-18 14:21:59 +0800

    Turn on output logging for screen
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

--- kuiper_gui_apps.sh
+++ kuiper_gui_apps.sh
@@ -5,32 +5,32 @@ cd /boot/ && echo analog | sudo -S mkdir -p gui-screenshots/
 
 screen --version
 echo analog | sudo -S apt-get install screen
-screen -S iio-osc -dm osc
+screen -S iio-osc -L -Logfile osc.txt -dm osc
 sleep 10
 scrot 'iio-oscilloscope_%Y-%m-%d.png' -e 'mv $f /boot/gui-screenshots/'
 sleep 2
 screen -X -S iio-osc kill
 sleep 2
-screen -S gnu -dm gnuradio-companion
+screen -S gnu -L -Logfile gnu-radio.txt -dm gnuradio-companion
 sleep 45
 scrot 'gnuradio_%Y-%m-%d.png' -e 'mv $f /boot/gui-screenshots/'
 sleep 2
 screen -X -S gnu kill
 sleep 2
-screen -S colorimeter -dm adi_colorimeter
+screen -S colorimeter -L -Logfile colorimeter.txt -dm adi_colorimeter
 sleep 10
 scrot 'adi-colorimeter_%Y-%m-%d.png' -e 'mv $f /boot/gui-screenshots/'
 sleep 2
 screen -X -S colorimeter kill
 sleep 2
-screen -S browser -dm chromium-browser
+screen -S browser -L -Logfile browser.txt -dm chromium-browser
 sleep 20
 scrot 'browser_%Y-%m-%d.png' -e 'mv $f /boot/gui-screenshots/'
 sleep 2
 screen -X -S browser kill
 sleep 2
 CMD='flatpak run org.adi.Scopy'
-screen -S scopy -dm $CMD
+screen -S scopy -L -Logfile scopy.txt -dm $CMD
 sleep 10
 scrot 'scopy_%Y-%m-%d.png' -e 'mv $f /boot/gui-screenshots/'
 sleep 2
