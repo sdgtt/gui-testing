11d29589a1d8591e4e31a90f5e8f82141c46022c
Say <clarissa.septimo@analog.com>
2024-01-26 15:28:03 +0800

    add sleep time

--- test/gnuradio/test_adi_gnuradio.py
+++ test/gnuradio/test_adi_gnuradio.py
@@ -42,7 +42,7 @@ class TestADIGnuradio:
         main_window = self.gui.find_window("untitled - GNU Radio Companion")
         # center on screen
         self.gui.set_window_center(main_window)
-        time.sleep(20)
+        time.sleep(30)
         self.gui.controller.screenshot("results/test_open_app.png")
         assert self.gui.controller.locateCenterOnScreen("ref_test_open_app.png", grayscale=True, confidence=0.7)
 
