df2679a9851c87c057991189d238c24c92ed243b
Say <clarissa.septimo@analog.com>
2024-01-26 15:17:20 +0800

    print title and add time sleep

--- test/gnuradio/test_adi_gnuradio.py
+++ test/gnuradio/test_adi_gnuradio.py
@@ -36,13 +36,13 @@ class TestADIGnuradio:
             app_name="gnuradio",
             path="/usr/bin/gnuradio-companion",
         )
-        time.sleep(15)
-        # print([ self.gui.get_window_title(w) for w in self.gui.get_open_windows() ])
+        time.sleep(20)
+        print([ self.gui.get_window_title(w) for w in self.gui.get_open_windows() ])
         # find_main screen
         main_window = self.gui.find_window("untitled - GNU Radio Companion")
         # center on screen
         self.gui.set_window_center(main_window)
-        time.sleep(10)
+        time.sleep(15)
         assert self.gui.controller.locateCenterOnScreen("ref_test_open_app.png", grayscale=True, confidence=0.7)
         self.gui.controller.screenshot("results/test_open_app.png")
 
