5d95190c29c1970bd61f95e817569607228ed672
Say <clarissa.septimo@analog.com>
2024-02-07 13:00:57 +0800

    gnutesting1

--- test/gnuradio/test_adi_gnuradio.py
+++ test/gnuradio/test_adi_gnuradio.py
@@ -41,7 +41,7 @@ class TestADIGnuradio:
         # find_main screen
         main_window = self.gui.find_window("*untitled - GNU Radio Companion")
         # center on screen
-        self.gui.set_window_center(main_window)
+        # self.gui.set_window_center(main_window)
         time.sleep(30)
         assert self.gui.controller.locateAllOnScreen("ref_test_open_app.png", grayscale=True, confidence=0.5)
         self.gui.controller.screenshot("results/test_open_app.png")
