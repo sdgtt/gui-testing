76c45e40301f15783d937e12ee721dec98e5f6f3
Say <clarissa.septimo@analog.com>
2024-02-07 11:49:22 +0800

    get titlepage only

--- test/gnuradio/test_adi_gnuradio.py
+++ test/gnuradio/test_adi_gnuradio.py
@@ -39,12 +39,12 @@ class TestADIGnuradio:
         time.sleep(delay)
         print([ self.gui.get_window_title(w) for w in self.gui.get_open_windows() ])
         # find_main screen
-        main_window = self.gui.find_window("untitled - GNU Radio Companion")
+        # main_window = self.gui.find_window("untitled - GNU Radio Companion")
         # center on screen
         # self.gui.set_window_center(main_window)
-        time.sleep(delay)
-        self.gui.controller.screenshot("results/test_open_app.png")
-        self.gui.alert('Try alert message function')
+        # time.sleep(delay)
+        # self.gui.controller.screenshot("results/test_open_app.png")
+        # self.gui.alert('Try alert message function')
         # assert self.gui.controller.locateAllOnScreen("ref_test_open_app.png", grayscale=True, confidence=0.5)
 
 
