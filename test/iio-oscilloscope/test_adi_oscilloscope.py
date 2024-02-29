82c962b1336561ab5f87a008419935244d7c0c54
Say <clarissa.septimo@analog.com>
2024-02-29 09:44:30 +0800

    screenshot screen

--- test/iio-oscilloscope/test_adi_oscilloscope.py
+++ test/iio-oscilloscope/test_adi_oscilloscope.py
@@ -40,17 +40,18 @@ class TestIIOOscilloscope:
         for w in self.gui.get_open_windows():
             if w:   
                 print(self.gui.get_window_title(w))
-                print(self.gui.get_window_geometry(w))
-                time.sleep(delay)
-                # print(self.gui.set_window_position(w))
-        try:
-            self.gui.find_window("ADI IIO Oscilloscope")
-            self.gui.find_window("ADI IIO Oscilloscope - Capture1")
-        except:
-            print("Application window not found")
-        else:
-            self.gui.find_window("ADI IIO Oscilloscope - Capture1")
-            self.gui.find_window("ADI IIO Oscilloscope - Capture1")
+        #         print(self.gui.get_window_geometry(w))
+        time.sleep(delay)
+        self.gui.controller.screenshot("results/test_open_a_app.png")
+        #         # print(self.gui.set_window_position(w))
+        # try:
+        #     self.gui.find_window("ADI IIO Oscilloscope")
+        #     self.gui.find_window("ADI IIO Oscilloscope - Capture1")
+        # except:
+        #     print("Application window not found")
+        # else:
+        #     self.gui.find_window("ADI IIO Oscilloscope - Capture1")
+        #     self.gui.find_window("ADI IIO Oscilloscope - Capture1")
 
         
         
