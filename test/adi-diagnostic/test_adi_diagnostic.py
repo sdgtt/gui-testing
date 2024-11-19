1c5c95d7eda3979eb63f8490de443bea5076840f
cseptimo <clarissa.septimo@analog.com>
2024-11-20 06:50:20 +0800

    added gui click functions

--- test/adi-diagnostic/test_adi_diagnostic.py
+++ test/adi-diagnostic/test_adi_diagnostic.py
@@ -42,41 +42,37 @@ class TestDiagnostic:
         for w in self.gui.get_open_windows():
             if w:
                 print(self.gui.get_window_title(w))
-        # self.gui.controller.press('win')
-        # assert self.gui.controller.locateCenterOnScreen("ref/ref_test_open_win.png", grayscale=True, confidence=0.5)
-        # self.gui.controller.screenshot("results/test_open_win.png")
-
         print("Test build: Check application title")
-        # Find main screen
-        #found_window = None
-        # for w in self.gui.get_open_windows():
-        #     if w:
-        #         print(self.gui.get_window_title(w))
-        # time.sleep(delay)
-        # main_window = self.gui.find_window("Generate Diagnostic Report")
-        # self.gui.set_window_center(main_window)
-        # print("Test build: Done main window")
-        # time.sleep(delay)
-        # assert self.gui.controller.locateCenterOnScreen("ref_test_open_diagnostic.png", grayscale=True, confidence=0.5)
-        # self.gui.controller.screenshot("results/test_open_diagnostic.png")
+        #Find main screen
+        found_window = None
+        for w in self.gui.get_open_windows():
+            if w:
+                print(self.gui.get_window_title(w))
+        time.sleep(delay)
+        main_window = self.gui.find_window("Generate Diagnostic Report")
+        self.gui.set_window_center(main_window)
+        print("Test build: Done main window")
+        time.sleep(delay)
+        assert self.gui.controller.locateCenterOnScreen("ref_test_open_diagnostic.png", grayscale=True, confidence=0.9)
+        self.gui.controller.screenshot("results/test_open_diagnostic.png")
       
         
-    # def test_run_button(self,delay):
+    def test_run_button(self,delay):
         '''Test if capture works by clicking the checkbox button'''
-        noneBtn = self.gui.controller.locateCenterOnScreen("ref_test_click_none_button.png", grayscale=True, confidence=0.5)
+        noneBtn = self.gui.controller.locateCenterOnScreen("ref_test_click_none_button.png", grayscale=True, confidence=0.9)
         assert noneBtn
         self.gui.controller.click(noneBtn)
         time.sleep(5)
         self.gui.controller.screenshot("results/test_none_button.png")   
         print("Test build: Done None button")
         time.sleep(delay)
-        analogItem = self.gui.controller.locateCenterOnScreen("ref_test_run_button.png", grayscale=True, confidence=0.5)
+        analogItem = self.gui.controller.locateCenterOnScreen("ref_test_run_button.png", grayscale=True, confidence=0.9)
         assert analogItem
         self.gui.controller.click(analogItem)
         time.sleep(5)
         self.gui.controller.screenshot("results/test_analog_item.png") 
         print("Test build: Done analog item")
-        generateBtn = self.gui.controller.locateCenterOnScreen("ref_test_click_generate_button.png", grayscale=True, confidence=0.5)
+        generateBtn = self.gui.controller.locateCenterOnScreen("ref_test_click_generate_button.png", grayscale=True, confidence=0.9)
         assert generateBtn
         self.gui.controller.click(generateBtn)
         time.sleep(5)
