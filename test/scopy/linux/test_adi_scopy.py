4c095e62273ea38a9fd8dee7b72009fe1a5c4740
cseptimo <clarissa.septimo@analog.com>
2024-11-20 08:05:11 +0800

    update scopy

--- test/scopy/linux/test_adi_scopy.py
+++ test/scopy/linux/test_adi_scopy.py
@@ -45,36 +45,5 @@ class TestScopy:
                 print(self.gui.get_window_title(w))
         print(self.gui.get_window_title(w))
         time.sleep(delay)
-        #find_small_window
-    #     time.sleep(delay)
-    #     small_window = self.gui.find_window("Scopy")
-    #     self.gui.set_window_center(small_window)
-    #     print("Test build: Done small window")
-    #     time.sleep(delay)
-    #     assert self.gui.controller.locateCenterOnScreen("ref_test_open_adi_iio_oscilloscope_app.png", grayscale=True, confidence=0.5)
-    #     self.gui.controller.screenshot("results/test_open_adi_iio_oscilloscope_app.png")
-    #     # find_main screen
-    #     main_window = self.gui.find_window("ADI IIO Oscilloscope - Capture1")
-    #     self.gui.set_window_center(main_window)
-    #     time.sleep(delay)
-    #     assert self.gui.controller.locateCenterOnScreen("ref_test_open_adi_iio_oscilloscope-capture1_app.png", grayscale=True, confidence=0.5)
-    #     self.gui.controller.screenshot("results/test_open_adi_iio_oscilloscope-capture1_app.png")
-    #     print("Test build: Done main window")
-        
-    # def test_play_button(self,delay):
-    #     '''Test if capture works by clicking the checkbox button'''
-    #     checkboxBtn = self.gui.controller.locateCenterOnScreen("ref_test_enable_all_checkbox.png", grayscale=True, confidence=0.9)
-    #     assert checkboxBtn
-    #     self.gui.controller.click(checkboxBtn)
-    #     time.sleep(5)
-    #     self.gui.controller.screenshot("results/test_checkbox_button.png")   
-    #     print("Test build: Done checkbox")
-    #     time.sleep(delay)
-    #     runBtn = self.gui.controller.locateCenterOnScreen("ref_test_run_button.png", grayscale=True, confidence=0.5)
-    #     assert runBtn
-    #     self.gui.controller.click(runBtn)
-    #     time.sleep(5)
-    #     self.gui.controller.screenshot("results/test_run_button.png") 
-    #     print("Test build: Done run button")
 
       
